b1f8d4c6f62822f106ea14f99cb0bee9e058c34e
Louis Didomenicis <68966648+ljboy99@users.noreply.github.com>
2023-10-15 22:16:31 -0400

    Update bootsearch.py
    - Added tkinter Gui
    - While loop so Edge stays open

--- bootsearch.py
+++ bootsearch.py
@@ -1,12 +1,34 @@
-import time
+import time, sys
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+import tkinter as tk
+from functools import partial
 from selenium.webdriver.support.ui import Select
+stylenumber = ""
 
-stylenumber = input("""Please provide a style number.
-Ex. 8111 - Iron Ranger, 875 - Classic Moc
-""")
+myroot = tk.Tk()
+myroot.geometry('250x80')
+myroot.title("RW Search")
+
+def assignStyle():
+    global stylenumber
+    stylenumber = userinentry.get()
+    myroot.destroy()
+    if stylenumber == "":
+        print("No style was defined. Aborting...")
+        sys.exit()
+    return stylenumber
+
+userinlabel = tk.Label(myroot, text = "Enter style number / name")
+userinentry = tk.Entry(myroot)
+submitButton = tk.Button(myroot, text = "SEARCH", command=assignStyle)
+
+userinlabel.pack()
+userinentry.pack()
+submitButton.pack()
+
+myroot.mainloop()
 
 driver = webdriver.Edge() #select Edge as a browser
 driver.get("https://www.redwingshoes.com")
@@ -16,4 +38,6 @@ searchbutton.click()
 #searchbar = driver.find_element(By.CLASS_NAME, "js-search-container c-search-form site-search")
 searchbar = driver.find_element(By.ID, "search")
 searchbar.send_keys(stylenumber, Keys.ENTER)
-time.sleep(10)
+
+while True:
+   time.sleep(1)
